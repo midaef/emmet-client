18aac65f8961d7383667c87a0a54b1807189c8d8
midaef <danil.present@mail.ru>
2021-04-13 15:33:06 +0800

    Update cli.go

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"bufio"
 	"fmt"
+	tm "github.com/buger/goterm"
 	"github.com/midaef/emmet-client/internal/client"
 	"os"
 	"strings"
@@ -15,14 +16,18 @@ type AuthCLI interface {
 type UserCLI interface {
 	UpdatePassword()
 	CreateUser()
+	DeleteUser()
 }
 
 type RoleCLI interface {
-
+	CreateRole()
+	DeleteRole()
 }
 
 type DataCLI interface {
-
+	CreateValue()
+	DeleteValue()
+	GetValue()
 }
 
 type CLI struct {
@@ -71,17 +76,44 @@ func (c *CLI) CheckCommand(command string) {
 	case "create user":
 		c.UserCLI.CreateUser()
 	case "create role":
+		c.RoleCLI.CreateRole()
 	case "create value":
+		c.DataCLI.CreateValue()
 	case "delete user":
+		c.UserCLI.DeleteUser()
 	case "delete role":
+		c.RoleCLI.DeleteRole()
 	case "delete value":
+		c.DataCLI.DeleteValue()
 	case "get value":
+		c.DataCLI.GetValue()
 	case "get token":
-		fmt.Printf("Access token: %s\n", c.UserInfo.AccessToken)
+		token := fmt.Sprintf("Access token: %s", c.UserInfo.AccessToken)
+		fmt.Println("\n" + tm.Bold(token) + "\n")
 	case "help":
+		c.help()
 	default:
-		fmt.Printf("Command not found. Use help\n")
+		fmt.Printf("\n" + tm.Color(tm.Bold("Command not found. Use "), tm.RED) +
+			tm.Color(tm.Bold("help"), tm.GREEN) + "\n\n")
 	}
+}
+
+func (c *CLI) help() {
+	fmt.Printf("\n" + tm.Bold("Commands") + "\n")
+	
+	commands := []string{"create user", "create role", "create value", "delete user", "delete role", "delete value", "get value"}
+	info := []string{"new user with role", "new role with permissions", "new value with role",
+		"user will be deleted", "role will be deleted", "value will be deleted", "get value with key and permissions"}
+	generateHelp(commands, info, len(commands))
+}
+
+func generateHelp(commands []string, info []string, fields int) {
+	for i := 0; i < fields; i++ {
+		fmt.Printf("\n" + tm.Color(tm.Bold(commands[i] + " - "), tm.GREEN) +
+			tm.Bold(info[i]) + "\n")
+	}
+
+	fmt.Print("\n")
 }
 
 func Reader(fields []string) map[string]string {
